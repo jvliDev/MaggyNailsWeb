70320e09e7bbeaec505f6f8fd9166cc262c95c5d
JvliDev <Nova_Blue1@outlook.com>
2024-11-26 15:32:30 -0600

    commit7

--- JSFunctions.js
+++ JSFunctions.js
@@ -3,9 +3,10 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.height = "6rem";
+
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.height = "5rem";
   }
   prevScrollpos = currentScrollPos;
 }
